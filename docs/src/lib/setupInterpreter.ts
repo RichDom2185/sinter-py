5888544a9d7dd90d3825d6afd23efd4c31a29374
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-12-28 01:16:37 +0800

    Reduce duplication and coupling

--- docs/src/lib/setupInterpreter.ts
+++ docs/src/lib/setupInterpreter.ts
@@ -1,26 +1,32 @@
 import { PyodideInterface } from "pyodide";
-
-import instructions_handlers from "./interpreter-src/instructions/handlers.py?raw";
-import instructions_instructions from "./interpreter-src/instructions/instructions.py?raw";
-import instructions_primitives from "./interpreter-src/instructions/primitives.py?raw";
-
-import interpreter from "./interpreter-src/interpreter.py?raw";
-import utils from "./interpreter-src/utils.py?raw";
-
 import entrypoint from "./sinter-py.py?raw";
+
+const sinterLib = import.meta.glob("./**/*.py", {
+  query: "?raw",
+  import: "default",
+  eager: true,
+  base: "../../../src/",
+});
+
+function extractFoldersToCreate(files: Record<string, unknown>): Set<string> {
+  const folders = Object.keys(files).map((filePath) => {
+    const [_dot, ...parts] = filePath.split("/");
+    parts.pop(); // Remove filename
+    return parts.join("/");
+  });
+  return new Set(folders.filter((folder) => folder.length > 0));
+}
 
 export const setupInterpreter = (pyodide: PyodideInterface) => {
   // TODO: Add type annotation
   const fs = pyodide.FS;
-
-  fs.mkdir("instructions");
-  fs.writeFile("instructions/handlers.py", instructions_handlers);
-  fs.writeFile("instructions/instructions.py", instructions_instructions);
-  fs.writeFile("instructions/primitives.py", instructions_primitives);
-  fs.writeFile("interpreter.py", interpreter);
-  fs.writeFile("utils.py", utils);
+  const foldersToCreate = extractFoldersToCreate(sinterLib);
+  for (const folder of foldersToCreate) {
+    fs.mkdirTree(folder);
+  }
+  for (const [filePath, fileContent] of Object.entries(sinterLib)) {
+    fs.writeFile(filePath, fileContent as string);
+  }
+  // Overwrite entrypoint with ours for now
   fs.writeFile("main.py", entrypoint);
-
-  pyodide.pyimport("interpreter");
-  pyodide.pyimport("utils");
 };
